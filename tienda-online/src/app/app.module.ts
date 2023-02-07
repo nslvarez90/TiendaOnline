4bfc39493447aa944a550d9aacdba417cc5842a3
Nelson Sanchez <nsanchez@pyxelsolution.com>
2023-02-07 11:33:39 -0500

    Define router root app and add services

--- tienda-online/src/app/app.module.ts
+++ tienda-online/src/app/app.module.ts
@@ -1,18 +1,36 @@
+import { ConvertService } from './services/convert.service';
+import { GeneralService } from './services/general.service';
+import { UtilityService } from './services/utility.service';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InterceptorService } from './services/interceptor/interceptor.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: PathLocationStrategy,
+  },
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: InterceptorService,
+    multi: true,
+  },
+    UtilityService,
+    GeneralService,
+    ConvertService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
