10c73e333db36d96d220646a2d551f5b15e2886b
Nelson Sanchez <nsanchez@pyxelsolution.com>
2023-02-07 10:41:30 -0500

    Add constants to manage acces to apis and key to chanhe currency

---
+++ tienda-online/src/app/app.constans.ts
@@ -0,0 +1,19 @@
+/**
+* @author Nelson Sánchez Álvarez
+* @date 2023/02/07
+* @description DEFINITION OF CONSTANTS
+*/
+
+export const CONSTANTS = {
+    METHOD: {
+        GET: 'get',
+        GET2: 'get',
+        POST: 'post',
+        PUT: 'put',
+        DELETE: 'delete',
+        PATCH: 'patch',
+    },
+    CURRENCY: {
+        KEY: 'Yflzyb9AO5wDKI3T7dBfzoZD1eSprmSG'
+    }
+};
