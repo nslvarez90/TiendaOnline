de20bafa5557070d50b53ea6830a9253e72d6b76
Nelson Sanchez <nsanchez@pyxelsolution.com>
2023-02-07 11:37:02 -0500

    Define routes to make services call using observables

---
+++ tienda-online/src/app/proxies/proxy-services.routes.ts
@@ -0,0 +1,17 @@
+/**
+* @author Nelson Sánchez Alvarez
+* @date 2023/02/07
+*/
+
+
+
+export const  PROXY_SERVICES = {
+    exchange: {
+        convert: '/convert?to=',
+    },
+    products: {        
+        getAll: '/products',
+        getAllByCategory: '/products?category=',       
+
+    }
+};
